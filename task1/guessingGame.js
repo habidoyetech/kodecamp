656a4087d860a6047126c6bdc9fdd16364de329a
unknown <abidoyeabiodun97@gmail.com>
2023-09-02 10:59:20 +0100

    guessing game code

---
+++ task1/guessingGame.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const randomNumber = generateRandomNumber(1, 10);
+
+rl.question('Guess a number between 1 and 10: ', (userInput) => {
+  const guessedNumber = parseInt(userInput);
+  
+  if (guessedNumber === randomNumber) {
+    console.log('You have won!');
+  } else {
+    console.log(`You have lost! The random number generated is ${randomNumber}.`);
+  }
+
+  rl.close();
+});
