e09058dcfebde10ca5c5328cc9a4f196867ab67c
unknown <abidoyeabiodun97@gmail.com>
2023-09-09 10:51:57 +0100

    updated the function to continue running

--- task1/guessingGame.js
+++ task1/guessingGame.js
@@ -9,16 +9,27 @@ function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const randomNumber = generateRandomNumber(1, 10);
+function startGame() {
+  const randomNumber = generateRandomNumber(1, 10);
 
-rl.question('Guess a number between 1 and 10: ', (userInput) => {
-  const guessedNumber = parseInt(userInput);
-  
-  if (guessedNumber === randomNumber) {
-    console.log('You have won!');
-  } else {
-    console.log(`You have lost! The random number generated is ${randomNumber}.`);
-  }
+  rl.question('Guess a number between 1 and 10 (or type "exit" to quit): ', (userInput) => {
+    if (userInput.toLowerCase() === 'exit') {
+      console.log('Exiting the game. Goodbye!');
+      rl.close();
+    } else {
+      const guessedNumber = parseInt(userInput);
 
-  rl.close();
-});
+      if (guessedNumber === randomNumber) {
+        console.log('You have won!');
+      } else {
+        console.log(`You have lost! The random number generated is ${randomNumber}.`);
+      }
+
+      // Play the game again
+      startGame();
+    }
+  });
+}
+
+// Start the game initially
+startGame();
